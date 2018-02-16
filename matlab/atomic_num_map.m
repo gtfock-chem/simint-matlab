a6da19751bdda7769d31a6bd482b08815450d1b6
Edmond Chow <edmond@apiary>
2018-02-16 17:39:30 -0500

    atomic_num_map.m

---
+++ matlab/atomic_num_map.m
@@ -0,0 +1,10 @@
+function num = atomic_num_map(key)
+if     key == 'H'    num = 1;
+elseif key == 'He'   num = 2;
+elseif key == 'C'    num = 6;
+elseif key == 'N'    num = 7;
+elseif key == 'O'    num = 8;
+elseif key == 'F'    num = 9;
+elseif key == 'Al'   num = 13;
+elseif key == 'Cl'   num = 17;
+end
