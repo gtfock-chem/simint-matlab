6a654c1d855d863061d1426e41c7cbcb9286f8d4
Edmond Chow <echow@cc.gatech.edu>
2018-02-16 22:30:55 -0500

    modify driv_calculate_nai.m - compute one-electron matrices

--- examples/driv_calculate_nai.m
+++ examples/driv_calculate_nai.m
@@ -1,4 +1,5 @@
-shells = readmol('/home/edmond/now/simint-matlab/simint-v0.7/test/dat/water.sto-3g.mol');
+%shells = readmol('/home/edmond/now/simint-matlab/simint-v0.7/test/dat/water.sto-3g.mol');
+shells = readmol('/home/echow/simint-generator/outdir/test/dat/water.sto-3g.mol');
 
 % construct data structures for atoms in the molecule
 
@@ -21,24 +22,28 @@ if min(atomic_nums) < 1
   error('Missing atom information for at least one atom index.');
 end
 
+% v = calculate_nai(atomic_nums, xcoords, ycoords, zcoords, shells(3), shells(3))
 
-v = calculate_nai(atomic_nums, xcoords, ycoords, zcoords, shells(1), shells(1))
-
+% calculate core Hamiltonian
 
 am = [shells.am];
 nf = (am+1).*(am+2)/2; % number of functions
 nftot = sum(nf);
 off = [1 cumsum(nf)+1];
 
-nai_matrix = zeros(nftot,nftot);
+Hcore = zeros(nftot,nftot);
 nshell = length(shells);
 for i = 1:nshell
 for j = 1:nshell
-  v = calculate_nai(atomic_nums, xcoords, ycoords, zcoords, shells(i), shells(j));
-% v = reshape(v, nf(i), nf(j)); % check
+% v = calculate_ovlpi(shells(i), shells(j));
+  v = calculate_kei(shells(i), shells(j));
+  v = v + calculate_nai(atomic_nums, xcoords, ycoords, zcoords, ...
+                    shells(i), shells(j));
+% v = reshape(v, nf(i), nf(j)); % check; H2O is not a good enough test
   v = reshape(v, nf(j), nf(i));
   v = v';
-  nai_matrix(off(i):off(i+1)-1,off(j):off(j+1)-1) = ...
-  nai_matrix(off(i):off(i+1)-1,off(j):off(j+1)-1) + v;
+  Hcore(off(i):off(i+1)-1,off(j):off(j+1)-1) = ...
+  Hcore(off(i):off(i+1)-1,off(j):off(j+1)-1) + v;
 end
 end
+Hcore
